ca753eff60a2a2e58dd7cd24bb711588fdd4d3fd
Abd Al-Gader Sirag <abdalgadersir@gmail.com>
2024-06-12 18:40:48 +0200

    adding the api routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// testing routes
 Route::get('/user', function () {
     \Illuminate\Support\Facades\Auth::logout();
     \Illuminate\Support\Facades\Auth::loginUsingId(2);
@@ -12,6 +13,22 @@ Route::get('/user', function () {
 });
 
 
+//
 Route::middleware('auth:sanctum')->namespace('App\Http\Controllers')->group(function (){
     Route::apiResource('video','VideoController');
+    Route::apiResource('subject','SubjectController');
+    Route::get('logout','Auth\LoginController@logout')->name('logout');
+    Route::put('user/update','Auth\UserController@updateUser')->name('user.update');
+});
+
+// guest user
+Route::middleware('guest')->namespace('App\Http\Controllers')->group(function (){
+    Route::post('login','Auth\LoginController@login')->name('login');
+});
+
+// admin panel routes
+Route::middleware('admin')->namespace('App\Http\Controllers')->group(function (){
+    Route::post('register','Auth\UserController@register')->name('register');
+    Route::put('user/edit/{user}','Auth\UserController@editUser')->name('user.edit');
+    Route::delete('user/delete/{user}','Auth\UserController@deleteUser')->name('user.delete');
 });
